4af96ca6b33687d1f68d4d23f41282efe780951b
minhmanood <minhmanne0809@gmail.com>
2025-01-11 10:36:29 +0700

    create comon layout

---
+++ src/app.js
@@ -0,0 +1,34 @@
+import express from "express";
+import path from "path";
+import { title } from "process";
+const app = express();
+const port = 5001;
+const __dirname = path.resolve();
+app.use("/static", express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/src/views");
+app.get("/", (req, res) => {
+  res.render("pages/index",{title: "Home"});
+});
+app.get("/components", (req, res) => {
+  res.render("pages/components", { title: "components" });
+});
+app.get("/forms", (req, res) => {
+  res.render("pages/forms", { title: "forms" });
+});
+app.get("/icons", (req, res) => {
+  res.render("pages/icons",{title: "Icons"});
+});
+app.get("/notification", (req, res) => {
+  res.render("pages/notification",{title:"Notification"});
+});
+app.get("/tables", (req, res) => {
+  res.render("pages/tables", { title: "tables" });
+});
+app.get("/typography", (req, res) => {
+  res.render("pages/typography", { title: "typography" });
+});
+
+app.listen(port, function () {
+  console.log("http://localhost:" + port);
+});
